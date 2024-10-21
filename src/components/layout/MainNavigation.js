de450f22f22ef009a97088935168af0b880134f6
JPriC <JPrietoC@uxcale.com>
2024-10-21 17:37:39 +0200

    feature: navigation bar animation on scroll-down and scroll-up

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,10 +1,45 @@
 import { Link } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
+import { useEffect, useRef, useState } from "react";
 
 function MainNavigation() {
+  const [showNavigation, setShowNavigation] = useState(true);
+  const lastScrollY = useRef(0);
+  const ticking = useRef(false);
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+
+    if (!ticking.current) {
+      window.requestAnimationFrame(() => {
+        if (currentScrollY > lastScrollY.current && currentScrollY > 75) {
+          setShowNavigation(false);
+        } else {
+          setShowNavigation(true);
+        }
+        lastScrollY.current = currentScrollY;
+        ticking.current = false;
+      });
+
+      ticking.current = true;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className={classes.header} data-test="navigation-header">
+    <header
+      className={`${classes.header} ${
+        showNavigation ? classes.show : classes.hide
+      }`}
+      data-test="navigation-header"
+    >
       <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
